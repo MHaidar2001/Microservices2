74df450de2753b1823b770606ade2a79d8870e27
Mohamad Haidar <mohaidar551@outlook.de>
2021-02-19 21:05:01 +0100

    KW07

---
+++ Mannschaftsverwaltung/Models/Mannschaft.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mannschaftsverwaltung.Models
+{
+    public class Mannschaft
+    {
+        #region Eigenschaften
+        private int _ID;
+        private string __Mannschaftsname;
+        private string _Sportart;
+        private List<Person> _ListePerson;
+        
+        #endregion
+
+        #region Accessoren/Modifier
+        public int ID { get => _ID; set => _ID = value; }
+        public string Sportart { get => _Sportart; set => _Sportart = value; }
+        public string Mannschaftsname { get => __Mannschaftsname; set => __Mannschaftsname = value; }
+        public List<Person> ListePerson { get => _ListePerson; set => _ListePerson = value; }
+        #endregion
+
+        #region Konstruktoren
+        public Mannschaft()
+        {
+            this.Mannschaftsname = "FC Bonn";
+            ID = 0;
+            Sportart = "";
+            ListePerson = new List<Person>();
+            
+           
+
+
+        }
+
+        public Mannschaft(int id, string name, string sportart)
+        {
+            this.ID = id;
+            this.Sportart = sportart;
+            this.Mannschaftsname = name;
+        }
+        #endregion
+
+        #region Worker
+        #endregion
+    }
+}
---
+++ Mannschaftsverwaltung/Models/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mannschaftsverwaltung.Models
+{
+    public class Person
+    {
+        #region Eigenschaften
+        private int _id;
+        private string _name;
+        private string _Sportart;
+        private string _einsatzbereich;
+
+
+        #endregion
+
+        #region Accessoren/Modifier
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Sportart { get => _Sportart; set => _Sportart = value; }
+        public string Einsatzbereich { get => _einsatzbereich; set => _einsatzbereich = value; }
+
+        #endregion
+
+        #region Konstruktoren
+        public Person()
+        {
+            Id = 0;
+            Name = "";
+            Einsatzbereich = "";
+
+            Sportart = "";
+
+        }
+        public Person(int id, string name,  string einstaz)
+        {
+            Id = id;
+            Name = name;
+            Einsatzbereich = einstaz;
+
+
+        }
+        #endregion
+
+        #region Worker
+
+        #endregion
+
+    }
+}
